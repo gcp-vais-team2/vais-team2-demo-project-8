a7fc241c324806d07e2e99b155ad2b4005493d51
avinashjay1 <Avinash.Veershetty@bristlecone.com>
2025-07-22 18:28:43 +0530

    Update content of config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-config = {'db': 'http://www.knight-martinez.org/posts/postscategory.html', 'api_key': 'e4324d2b842909e47cfad600991e1134a02e9298'}
+CONFIG = {'project_id': 8, 'debug': True, 'owner': 'vais-team1'}
